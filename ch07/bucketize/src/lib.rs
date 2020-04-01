c971469548b5cc56d27783a9a7996731b7ca2bfc
Eduard Luhtonen <luhtonen@gmail.com>
2020-04-01 17:26:37 +0200

    add doc comments

--- ch07/bucketize/src/lib.rs
+++ ch07/bucketize/src/lib.rs
@@ -1,3 +1,47 @@
+// The first thing to do is to add some crate level documentation. We use //! for that.
+
+//! `bucketize` is a crate for slotting numerical values into buckets.
+//! To do this, create a `Bucketizer` and add your buckets to it,
+//! then use the `.bucketize()` method to get back the bucket a value fits into.
+//!
+//! # Example
+//! ```
+//! use bucketize::Bucketizer;
+//!
+//! let b = Bucketizer::new()
+//!     .bucket(Some(10.0), Some(20.0), 15.0)
+//!     .bucket(Some(5.0), Some(10.0), 7.5)
+//!     .bucket(None, Some(4.0), 0.0);
+//!
+//! assert_eq!(b.bucketize(12.34), Some(15.0));
+//! assert_eq!(b.bucketize(9999.99), None);
+//! ```
+
+// Now we need documentation for the Bucketizer struct.
+
+/// A `Bucketizer` holds the list of buckets you want to slot values into, and does
+/// the bucketization operation.
+///
+/// You can create one with `new()` and add buckets with chained `.bucket()` calls.
+/// These calls add buckets which are evaluated in order. For instance, if you add
+/// a bucket from 0 to 100 and then add a bucket from 2 to 50, nothing will ever
+/// get put in that second bucket.
+///
+/// Buckets are min-inclusive and max-exclusive. If a given value matches no bucket,
+/// `bucketize` returns `None`.
+///
+/// # Example
+/// ```
+/// use bucketize::Bucketizer;
+///
+/// let b = Bucketizer::new()
+///     .bucket(Some(10.0), Some(20.0), 15.0)
+///     .bucket(Some(5.0), Some(10.0), 7.5)
+///     .bucket(None, Some(4.0), 0.0);
+///
+/// assert_eq!(b.bucketize(12.34), Some(15.0));
+/// assert_eq!(b.bucketize(9999.99), None);
+/// ```
 #[derive(Clone, Debug, PartialEq)]
 pub struct Bucketizer {
     buckets: Vec<Bucket>
@@ -6,18 +50,54 @@ pub struct Bucketizer {
 type Bucket = (Option<f64>, Option<f64>, f64);
 
 impl Bucketizer {
+    /// Create a new `Bucketizer` with no buckets configured.
     pub fn new() -> Self {
         Bucketizer {
             buckets: Vec::new()
         }
     }
 
+    /// Add a new bucket to the `Bucketizer`. Consumes and returns the `Bucketizer` so
+    /// it can be chained.
+    ///
+    /// Buckets are evaluated in the order they are added.
+    ///
+    /// A value fits in a bucket if it is greater than or equal to `min` and less than
+    /// `max`, if each is present.
+    ///
+    /// # Examples
+    ///
+    /// Here, we create a `Bucketizer` with a single bucket matching any value 10
+    /// or greater.
+    ///
+    /// ```
+    /// # use bucketize::Bucketizer;
+    /// let b = Bucketizer::new().bucket(Some(10.0), None, 10.0);
+    ///
+    /// assert_eq!(b.bucketize(12.0), Some(10.0));
+    /// assert_eq!(b.bucketize(-10.0), None);
+    /// ```
+    ///
+    /// Here, we create a `Bucketizer` matching values from 0 to < 10 and from
+    /// 10 to infinity.
+    /// ```
+    /// # use bucketize::Bucketizer;
+    /// let b = Bucketizer::new()
+    ///     .bucket(Some(10.0), None, 10.0)
+    ///     .bucket(Some(0.0), Some(10.0), 5.0);
+    ///
+    /// assert_eq!(b.bucketize(4.132), Some(5.0));
+    /// assert_eq!(b.bucketize(12.0), Some(10.0));
+    /// assert_eq!(b.bucketize(-10.0), None);
+    /// ```
     pub fn bucket(self, min: Option<f64>, max: Option<f64>, value: f64) -> Self {
         let mut new = self;
         new.buckets.push((min, max, value));
         new
     }
 
+    /// Get the bucketized value of `input` based on the previously configured `buckets`
+    /// for this `Bucketizer`.
     pub fn bucketize(&self, input: f64) -> Option<f64> {
         for bucket in &self.buckets {
             match *bucket {
